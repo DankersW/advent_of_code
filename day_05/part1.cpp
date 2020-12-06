40f645bd3eacd6d236a87f52cf5d119f4bd31025
DankersW <wouter.dankers@skynet.be>
2020-12-06 20:17:06 +0100

    Found max seat

--- day_05/part1.cpp
+++ day_05/part1.cpp
@@ -2,22 +2,33 @@
 #include <string>
 #include <fstream>
 
-int get_row_nr(const std::string &row_str, int max_rows)
+int find_nr(const std::string &row_str, int max_rows)
 {
-    std::cout << row_str << std::endl;
-    return 0;
+    int max = max_rows, min = 0;
+    for(auto letter: row_str)
+    {        
+        if (letter == 'F' || letter == 'L') { max = max - ((max -min)/2) - 1; }
+        else if (letter == 'B' || letter == 'R') { min = min + ((max -min)/2) + 1; }
+    }
+    return min;
+}
+
+int get_seat(const std::string &input_line)
+{
+    int row = find_nr(input_line.substr(0,7), 127);
+    int col = find_nr(input_line.substr(7,9), 7);
+    return row * 8 + col;
 }
 
 int main()
 {
-    std::ifstream file("input_example.txt");
+    int max_seat = 0;
+    std::ifstream file("input_test.txt");
     std::string line;
     while (std::getline(file, line))
     {
-        std::cout << line << std::endl;
-
-        int row_nr = get_row_nr(line.substr(0,7), 127);
-
-        std::cout << std::endl;
+        int seat = get_seat(line);
+        if (seat > max_seat) { max_seat = seat; }
     }
+    std::cout << "max seat: " << max_seat << std::endl;
 }
