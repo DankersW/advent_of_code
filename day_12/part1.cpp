8c6661f70a740d6217e89e1159e608e9e3e58881
DankersW <wouter.dankers@skynet.be>
2020-12-13 20:48:52 +0100

    part 1 done

--- day_12/part1.cpp
+++ day_12/part1.cpp
@@ -10,24 +10,31 @@ struct Instruction
     int value;
 };
 
-char calc_direction(const char &starting_direction, int turning_degrees)
+int get_direction_index_left(int jump, int index)
+{
+    if (index - jump >= 0) { return index - jump; }
+    else { return index - jump + 4; }
+}
+
+int get_direction_index_right(int jump, int index)
+{
+    if (jump + index < 4) { return jump + index; }
+    else { return jump + index - 4; }
+}
+
+char calc_direction(const char &starting_direction, const char &heading, int turning_degrees)
 {
     char directions[4] = {'N', 'E', 'S', 'W'};
 
     int jumps = turning_degrees / 90;
     int index_start_direction = std::distance(directions, std::find(directions, directions + 4, starting_direction));
-  
-    if (jumps + index_start_direction < 4)
-    {
-        return directions[jumps + index_start_direction];
-    }
-    else
-    {
-        return directions[jumps + index_start_direction-4];
-    }
+
+    int direction_index = (heading == 'L') ? get_direction_index_left(jumps, index_start_direction) : 
+                                             get_direction_index_right(jumps, index_start_direction);
+    return directions[direction_index];
 }
 
-int follow_instructions(const std::vector<Instruction> &instructions, char starting_direction = 'E')
+int calc_manhatten_dist(const std::vector<Instruction> &instructions, char starting_direction = 'E')
 {
     int east_counter = 0;
     int north_counter = 0;
@@ -36,57 +43,27 @@ int follow_instructions(const std::vector<Instruction> &instructions, char start
 
     for (auto instr: instructions)
     {
-        std::cout << instr.action << "-" << instr.value << std::endl;
-
-        if (instr.action == 'N' || (instr.action == 'F' && direction == 'N'))
-        {
-            north_counter += instr.value;
-        }
-        else if (instr.action == 'S' || (instr.action == 'F' && direction == 'S'))
-        {
-            north_counter -= instr.value;
-        }
-        else if (instr.action == 'E' || (instr.action == 'F' && direction == 'E'))
-        {
-            east_counter += instr.value;
-        }
-        else if (instr.action == 'W' || (instr.action == 'F' && direction == 'W'))
-        {
-            east_counter -= instr.value;
-        }
-        else if (instr.action == 'L' || instr.action == 'R')
-        {
-            direction = calc_direction(direction, instr.value);
-        }
-        std::cout << "east: " << east_counter << "\t north: " << north_counter << std::endl;
+        if (instr.action == 'N' || (instr.action == 'F' && direction == 'N')) { north_counter += instr.value; }
+        else if (instr.action == 'S' || (instr.action == 'F' && direction == 'S')) { north_counter -= instr.value; }
+        else if (instr.action == 'E' || (instr.action == 'F' && direction == 'E')) { east_counter += instr.value; }
+        else if (instr.action == 'W' || (instr.action == 'F' && direction == 'W')) { east_counter -= instr.value; }
+        else if (instr.action == 'L' || instr.action == 'R') { direction = calc_direction(direction, instr.action, instr.value); }
     }
-
     return abs(north_counter) + abs(east_counter);
 }
-
 
 int main()
 {
     std::vector<Instruction> instructions;
-    std::ifstream file("input_example.txt");
+    std::ifstream file("input_test.txt");
     std::string line;
     while (std::getline(file, line))
     {
-        std::cout << line << std::endl;
         instructions.push_back({line[0], std::stoi(line.substr(1))});
     }
 
-    //int distance = follow_instructions(instructions);
-    //std::cout << "manhatten distance: " << distance << std::endl;
+    int distance = calc_manhatten_dist(instructions);
+    std::cout << "manhatten distance: " << distance << std::endl;
 
-    std::cout << calc_direction('N', 90) << std::endl;
-    std::cout << calc_direction('E', 90) << std::endl;
-    std::cout << calc_direction('S', 90) << std::endl;
-    std::cout << calc_direction('W', 90) << std::endl;
-    std::cout << calc_direction('N', 180) << std::endl;
-    std::cout << calc_direction('E', 180) << std::endl;
-    std::cout << calc_direction('S', 180) << std::endl;
-    std::cout << calc_direction('W', 180) << std::endl;
-    
     return 0;
 }
