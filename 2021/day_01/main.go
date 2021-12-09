c89ec2b83e64ad0e1c8e02802379726c69dbb042
DankersW <wouter.dankers@skynet.be>
2021-12-09 16:54:51 +0100

    part 1 done

---
+++ 2021/day_01/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.Info("hi")
+	content, _ := ioutil.ReadFile("data_1.txt")
+	lines := strings.Split(string(content), "\n")
+	log.Info(lines)
+
+	count := -1
+	prev_val := -1
+	for _, val := range lines {
+		number, _ := strconv.Atoi(val)
+		if number > prev_val {
+			count++
+		}
+		prev_val = number
+	}
+	log.Info(count)
+}
